09400fce653abecaef62041d1a9a0515df5246ce
Santtu99 <amk1013167@student.hamk.fi>
2025-10-20 15:14:38 +0300

    Update IkaOhjelma.java
    Lisätty mopon ja kevarin ajoluvat sekä täysi-ikäisyysviesti

--- IkaOhjelma.java
+++ IkaOhjelma.java
@@ -1,12 +1,30 @@
 public class IkaOhjelma {
     public static void main(String[] args) {
 
-        int ika = 10; // Testattava arvo
+        int ika = 16; // Kokeile eri arvoja
 
-        if (ika >= 0 && ika < 18) {
+        if (ika > 0 && ika < 18) {
             System.out.println("Olet alaikäinen");
+
+            // Mopon ajaminen 15-vuotiaana
+            if (ika >= 15) {
+                System.out.println("Saat ajaa mopoa");
+            }
+
+            // Kevarin ajaminen 16–17-vuotiaana
+            if (ika >= 16) {
+                System.out.println("Saat ajaa kevaria");
+            }
+
+        } else if (ika >= 18) {
+            // Täysi-ikäisyys ja auton ajaminen
+            if (ika == 18) {
+                System.out.println("Olet juuri tullut täysi-ikäiseksi ja saat ajaa autoa!");
+            } else {
+                System.out.println("Olet aikuinen");
+            }
         } else {
-            System.out.println("Olet aikuinen");
+            System.out.println("Virheellinen ikä!");
         }
     }
 }
