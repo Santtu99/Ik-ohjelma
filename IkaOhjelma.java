befd4a46653c688930f21d340a779104ec4fb8c6
Santtu99 <amk1013167@student.hamk.fi>
2025-10-20 15:10:30 +0300

    Perus homma
    Lisätty perusversio ikäohjelmasta (alaikäinen / aikuinen)

---
+++ IkaOhjelma.java
@@ -0,0 +1,12 @@
+public class IkaOhjelma {
+    public static void main(String[] args) {
+
+        int ika = 10; // Testattava arvo
+
+        if (ika >= 0 && ika < 18) {
+            System.out.println("Olet alaikäinen");
+        } else {
+            System.out.println("Olet aikuinen");
+        }
+    }
+}
